1c6dd560f1b284688c9aa189d433d13e1ce2bec4
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2013-11-12 18:54:02 -0700

    "Added a valid email regular expression and a test case for it"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,26 @@ describe User do
   	before { @user.name = "a" * 41}
   	it { should_not be_valid }
   end
+
+  describe "when email format is invalid" do
+  	it "should be invalid" do
+  		emails = %w[@webboard.com user.webboard.com user.com user@webboard,com user@web_board.com user@web+board.com]
+  		emails.each do |e|
+  			@user.email = e
+  			expect(@user).to_not be_valid
+  		end
+  	end
+  end
+
+  describe "when email format is valid" do
+  	it "should be valid" do
+  		emails = %w[user@webboard.com user@web.board.com user_1@webboard.com user@webboard.COM
+  		 user+2@webboard.org user.3@webboard.com]
+  		emails.each do |e|
+  			@user.email = e
+  			expect(@user).to be_valid
+  		end
+  	end
+  end
+
 end
