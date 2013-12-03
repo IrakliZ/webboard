4a5b25b6282a32ba95dc59fab9cdbc9b3caad0ec
Evan Roncevich <eroncevich@gmail.com>
2013-12-02 21:34:33 -0700

    sign-in Scenario Finished

---
+++ features/support/paths.rb
@@ -0,0 +1,23 @@
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  # When /^I go to (.+)$/ do |page_name|
+
+  def path_to(page_name)
+    case page_name
+    when /^homepage$/ then '/'
+    when /^sign-up$/ then '/signup'
+    #when /^profile$/ then '/users/'
+
+    else
+      begin
+        page_name =~ /^the (.*) page$/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue NoMethodError, ArgumentError
+        raise "Can't find path"
+      end
+    end
+  end
+end
+
+World(NavigationHelpers)
