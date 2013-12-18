27f20810248820649b8a64196a02924cca07a9b9
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2013-12-17 20:51:03 -0700

    Removed max length of board to test post on heroku

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ActiveRecord::Base
 	belongs_to :user
-  validates :title, presence: true, length: { maximum: 40 }
+  validates :title, presence: true#, length: { maximum: 40 }
 	validates :user_id, presence: true
 end
